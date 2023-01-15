19a12c3bceb49003752113075798b0ee28ae0d1f
Professsional <63961221+ItsProfessional@users.noreply.github.com>
2023-01-15 18:15:07 +0530

    Clean log

--- src/main/java/ru/vidtu/ksyxis/Ksyxis.java
+++ src/main/java/ru/vidtu/ksyxis/Ksyxis.java
@@ -9,7 +9,5 @@ public class Ksyxis implements ModInitializer {
 	public static final Logger LOG = LogManager.getLogger("Ksyxis");
 	@Override
 	public void onInitialize() {
-		LOG.warn("Ksyxis will speedup your world loading. :P");
-		LOG.warn("BUT IT MAY CAUSE ISSUES (INCLUDING COMPAT). DON'T ASK OTHER MODS' AUTHORS FOR COMPAT!");
 	}
 }

ce425b9b18d44c7eaab9d3fc1a0788e00dcb6554
VidTu <imvidtu@proton.me>
2023-12-13 16:01:30 +0300

    chore: Improve build files

--- loaders/build.gradle.kts
+++ loaders/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
 java.toolchain.languageVersion = JavaLanguageVersion.of(8)
+group = "ru.vidtu.ksyxis.loaders"
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
